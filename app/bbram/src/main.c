32538f8205c6d3477f794f8f0487d6dab6e7b9bd
Luca Hung <YCHUNG0@nuvoton.com>
2023-11-07 09:43:39 +0800

    app: bbram: refactor bbram and add x4 overlay
    As title.
    
    Signed-off-by: Luca Hung <YCHUNG0@nuvoton.com>

--- app/bbram/src/main.c
+++ app/bbram/src/main.c
@@ -17,76 +17,84 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main);
 
-#define MAX_ARGUMNETS 3
-#define MAX_ARGU_SIZE 10
-static uint8_t arguments[MAX_ARGUMNETS][MAX_ARGU_SIZE];
-struct k_event bbram_event;
-
-#define TASK_STACK_SIZE         1024
-#define PRIORITY                7
 static const struct device *const bbram_dev = DEVICE_DT_GET(DT_NODELABEL(bbram));
 
-static struct k_thread temp_id;
-K_THREAD_STACK_DEFINE(temp_stack, TASK_STACK_SIZE);
+static uint32_t offs, size;
+static uint8_t buff[256];
 
-static void bbram_validation_func(void *dummy1, void *dummy2, void *dummy3)
+static void npcx_check_bbram_sts(void)
 {
-	uint32_t events, offs, size;
-	uint8_t buff[256];
+	/* check bbram data valid */
+	do {
+		offs = bbram_check_invalid(bbram_dev);
+		LOG_INF("[%d]IBBR", offs);
+	} while (offs);
+	if (offs == 0)
+		LOG_INF("[PASS] BBRAM data is valid");
+	else
+		LOG_INF("[FAIL] VSBY/VBAT is invalid");
 
-	offs = 0;
-	size = 0;
-	k_event_init(&bbram_event);
-	while (true) {
-		events = k_event_wait(&bbram_event, 0xFFF, true, K_FOREVER);
-		switch (events) {
-		case 0x001: /* no argu */
-			break;
-		case 0x002:
-			if (!strcmp("chk_valid", arguments[0])) {
-				do {
-					offs = bbram_check_invalid(bbram_dev);
-					LOG_INF("[%d]IBBR", offs);
-				} while (offs);
-			}
-			if (!strcmp("chk_sbypw", arguments[0])) {
-				LOG_INF("[%d]SBY", bbram_check_standby_power(bbram_dev));
-			}
-			if (!strcmp("chk_pw", arguments[0])) {
-				LOG_INF("[%d]PW", bbram_check_power(bbram_dev));
-			}
-			if (!strcmp("chk_sz", arguments[0])) {
-				bbram_get_size(bbram_dev, &size);
-				LOG_INF("[%d]", size);
-			}
-			if (!strcmp("write", arguments[0])) {
-				if (!bbram_write(bbram_dev, (size_t)offs, (size_t)size, buff))
-					LOG_INF("[PASS] Write");
-				else
-					LOG_INF("[FAIL] Write");
-			}
-			if (!strcmp("read", arguments[0])) {
-				if (!bbram_read(bbram_dev, (size_t)offs, (size_t)size, buff))
-					LOG_INF("[PASS] Read");
-				else
-					LOG_INF("[FAIL] Read");
-			}
-			break;
-		case 0x004:
-			if (!strcmp("offs", arguments[0]))
-				offs = atoi(arguments[1]);
-			if (!strcmp("size", arguments[0]))
-				size = atoi(arguments[1]);
-			if (!strcmp("idxR", arguments[0]))
-				LOG_INF("%d:[%d]", atoi(arguments[1]), buff[atoi(arguments[1])]);
-			break;
-		case 0x008:
-			if (!strcmp("idxW", arguments[0]))
-				buff[atoi(arguments[1])] = (size_t)atoi(arguments[2]);
-			break;
-		}
-	}
+	/* check voltage standby / backup */
+	if (bbram_check_standby_power(bbram_dev) == 0)
+		LOG_INF("[PASS] VSBY/VBAT is on");
+	else
+		LOG_INF("[FAIL] VSBY/VBAT is on");
+
+	/* check VCC1 failure */
+	if (bbram_check_power(bbram_dev) == 0)
+		LOG_INF("[PASS] VCC1 is on");
+	else
+		LOG_INF("[FAIL] VCC1 is on");
 }
+
+static void npcx_get_size(void)
+{
+	bbram_get_size(bbram_dev, &size);
+	LOG_INF("[%d]", size);
+}
+
+static void npcx_write(void)
+{
+	/* buff -> bbram */
+	if (!bbram_write(bbram_dev, (size_t)offs, (size_t)size, buff))
+		LOG_INF("[PASS] Write");
+	else
+		LOG_INF("[FAIL] Write");
+}
+
+static void npcx_read(void)
+{
+	/* bbram -> buff */
+	if (!bbram_read(bbram_dev, (size_t)offs, (size_t)size, buff))
+		LOG_INF("[PASS] Read");
+	else
+		LOG_INF("[FAIL] Read");
+}
+
+static void npcx_bbram_init(const struct shell *shell, size_t argc, char **argv)
+{
+	offs = strtoul(argv[1], NULL, 0);
+	size = strtoul(argv[2], NULL, 0);
+
+	LOG_INF("offset %d, size %d", offs, size);
+}
+
+static void bbram_idxR(const struct shell *shell, size_t argc, char **argv)
+{
+	/* Read buffer at idx */
+	int offset = strtoul(argv[1], NULL, 0);
+
+	LOG_INF("%d:[%d]", offset, buff[offset]);
+}
+static void bbram_idxW(const struct shell *shell, size_t argc, char **argv)
+{
+	/* Write buffer at idx */
+	int offset = strtoul(argv[1], NULL, 0);
+	int data = strtoul(argv[2], NULL, 0);
+
+	buff[offset] = data;
+}
+
 
 int main(void)
 {
@@ -95,32 +103,20 @@ int main(void)
 		return 0;
 	}
 	LOG_INF("[PASS]: BBRAM ready");
-	k_thread_create(&temp_id, temp_stack, TASK_STACK_SIZE,
-			bbram_validation_func, NULL, NULL, NULL, PRIORITY,
-			K_INHERIT_PERMS, K_FOREVER);
-	k_thread_name_set(&temp_id, "ps2 Validation");
-	k_thread_start(&temp_id);
-	return 0;
-}
-
-static int bbram_command(const struct shell *shell, size_t argc, char **argv)
-{
-	int i, evt;
-
-	evt = 1;
-	for (evt = 1, i = 1; i < argc; i++) {
-		strcpy(arguments[i-1], argv[i]);
-		evt <<= 1;
-	}
-	k_event_post(&bbram_event, evt);
 	return 0;
 }
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_bbram,
-	SHELL_CMD_ARG(c0, NULL, "bbram c0", bbram_command, 1, 0),
-	SHELL_CMD_ARG(c1, NULL, "bbram c1 arg0", bbram_command, 2, 0),
-	SHELL_CMD_ARG(c2, NULL, "bbram c2 arg0 arg1", bbram_command, 3, 0),
-	SHELL_CMD_ARG(c3, NULL, "bbram c3 arg0 arg1 arg2", bbram_command, 4, 0),
+	SHELL_CMD_ARG(init, NULL, "bbram init <offset> <size> : set bbram offset and size",
+		npcx_bbram_init, 3, 0),
+	SHELL_CMD_ARG(chk_sts, NULL, "bbram chk_sts : check bbram status",
+		npcx_check_bbram_sts, 1, 0),
+	SHELL_CMD_ARG(chk_sz, NULL, "bbram chk_sz : check size", npcx_get_size, 1, 0),
+	SHELL_CMD_ARG(write, NULL, "bbram write : buff->bbram", npcx_write, 1, 0),
+	SHELL_CMD_ARG(read, NULL, "bbram read : bbram->buff ", npcx_read, 1, 0),
+	SHELL_CMD_ARG(idxR, NULL, "bbram idxR <idx>: Read buff at idx", bbram_idxR, 2, 0),
+	SHELL_CMD_ARG(idxW, NULL, "bbram idxW <idx> <data>: Write data to buff at idx",
+		bbram_idxW, 3, 0),
 	SHELL_SUBCMD_SET_END /* Array terminated. */
 );
 SHELL_CMD_REGISTER(bbram, &sub_bbram, "BBRAM validation commands", NULL);
